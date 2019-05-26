ca78be3b1aa7ca5ca3e6321eacd9648962a63608
shushiqi <1539588925@qq.com>
2019-05-26 23:06:06 +0800

    内容组件

---
+++ miniprogram/components/contents/content.js
@@ -0,0 +1,25 @@
+// components/contents/content.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    show: true
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onClose() {
+      this.setData({ show: false });
+    }
+  }
+})
