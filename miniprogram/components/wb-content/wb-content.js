c3360d815c66b0a7ca38c3b3043d9dfbd7d98594
shushiqi <1539588925@qq.com>
2019-06-27 09:54:06 +0800

    组件化

---
+++ miniprogram/components/wb-content/wb-content.js
@@ -0,0 +1,32 @@
+// components/wbcontent/wb-content.js
+const Weibo = require('../../api/weibo')
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    weiboList: []
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    function(options) {
+      Weibo.weibo().then(data => {
+        console.log(data);
+        this.setData({
+          weiboList: data.data.weiboList
+        })      
+      })
+      
+    }
+  }
+})
